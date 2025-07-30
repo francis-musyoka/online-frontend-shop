84ee5c9ec5025ef42cb54518ac4942f67398ef74
francis-musyoka <mfrankmusyoka@gmail.com>
2025-07-30 12:35:10 +0300

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,91 +1,97 @@
 import './App.css'
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes, } from 'react-router-dom'
-import Home from './Features/customers/pages/Home';
+import { PATH_URL } from './constant';
+
 import Categories from './Features/customers/pages/Categories';
 import SignIn from './Features/customers/pages/SignIn';
-import Cart from './Features/customers/pages/Cart';
 import SignUp from './Features/customers/pages/SignUp';
 import Layout from './Features/customers/component/Layout';
-import Favourite from './Features/customers/pages/Account/Favourite';
-import Account from './Features/customers/pages/Account/Account';
-import Profile from './Features/customers/pages/Account/Profile';
-import Orders from './Features/customers/pages/Account/Orders';
-import { PATH_URL } from './constant';
-import ForgotPassword from '../src/Features/customers/pages/ForgotPassword';
-import Login from './Features/sellers/pages/Login';
-import SellDashBoard from './Features/sellers/pages/SellDashBoard';
-import AddProduct from './Features/sellers/pages/AddProduct';
-import EditProduct from './Features/sellers/pages/EditProduct';
-import ShopProfile from './Features/sellers/pages/ShopProfile';
-import MyProducts from './Features/sellers/pages/MyProducts';
-import CreateAccount from './Features/sellers/pages/CreateAccount';
 import RequireAuth from './Features/customers/component/RequireAuth';
 import ToastProvider from './context/ToastContext';
-import NotFound from './components/NotFound';
-import ResetPassword from '../src/Features/customers/pages/ResetPassword';
-import ForgotPasswordShop from './Features/sellers/pages/ForgotPasswordShop';
-import ResetShopPassword from './Features/sellers/ResetShopPassword';
 import ShopRequireAuth from './Features/sellers/components/ShopRequireAuth';
-import ProductDetails from './Features/customers/pages/ProductDetails';
-import Addresses from './Features/customers/pages/Account/Addresses';
-import CheckOut from './Features/customers/pages/CheckOut';
-import AddCategory from './Features/sellers/pages/AddCategory';
-import OrderSummary from './Features/customers/pages/orderSummary';
-import Email from './Features/customers/pages/email';
-import AddAddress from './Features/customers/component/AddAddress';
+import Spinning from './components/Spinning';
+
+// Lazy loaded components
+const Home = lazy(() => import('./Features/customers/pages/Home'));
+const ProductDetails = lazy(() => import('./Features/customers/pages/ProductDetails'));
+const Cart = lazy(() => import('./Features/customers/pages/Cart'))
+const Favourite = lazy(() => import('./Features/customers/pages/Account/Favourite'));
+const Account = lazy(() => import('./Features/customers/pages/Account/Account'));
+const Profile = lazy(() => import('./Features/customers/pages/Account/Profile'));
+const Orders = lazy(() => import('./Features/customers/pages/Account/Orders'));
+const Addresses = lazy(() => import('./Features/customers/pages/Account/Addresses'));
+const AddAddress = lazy(() => import('./Features/customers/component/AddAddress'));
+const CheckOut = lazy(() => import('./Features/customers/pages/CheckOut'));
+const OrderSummary = lazy(() => import('./Features/customers/pages/orderSummary'));
+const Email = lazy(() => import('./Features/customers/pages/email'));
+const ForgotPassword = lazy(() => import('../src/Features/customers/pages/ForgotPassword'));
+const ResetPassword = lazy(() => import('../src/Features/customers/pages/ResetPassword'));
+const NotFound = lazy(() => import('./components/NotFound'));
+const Login = lazy(() => import('./Features/sellers/pages/Login'));
+const SellDashBoard = lazy(() => import('./Features/sellers/pages/SellDashBoard'));
+const AddProduct = lazy(() => import('./Features/sellers/pages/AddProduct'));
+const EditProduct = lazy(() => import('./Features/sellers/pages/EditProduct'));
+const ShopProfile = lazy(() => import('./Features/sellers/pages/ShopProfile'));
+const MyProducts = lazy(() => import('./Features/sellers/pages/MyProducts'));
+const CreateAccount = lazy(() => import('./Features/sellers/pages/CreateAccount'));
+const ForgotPasswordShop = lazy(() => import('./Features/sellers/pages/ForgotPasswordShop'));
+const ResetShopPassword = lazy(() => import('./Features/sellers/ResetShopPassword'));
+const AddCategory = lazy(() => import('./Features/sellers/pages/AddCategory'));
 
 function App() {
 
     return (
         <>
             <BrowserRouter>
-                <ToastProvider>
-                    <Routes>
-                        {/* customers */}
-                        <Route path={PATH_URL.HOME} element={<Layout />}>
+                <Suspense fallback={<Spinning />}>
+                    <ToastProvider>
+                        <Routes>
+                            {/* customers */}
+                            <Route path={PATH_URL.HOME} element={<Layout />}>
+                                <Route path={PATH_URL.HOME} element={<Home />} />
+                                <Route path={PATH_URL.CATEGORIES} element={<Categories />} />
+                                <Route path={PATH_URL.SIGN_IN} element={<SignIn />} />
+                                <Route path={PATH_URL.CART} element={<Cart />} />
 
-                            <Route path={PATH_URL.HOME} element={<Home />} />
-                            <Route path={PATH_URL.CATEGORIES} element={<Categories />} />
-                            <Route path={PATH_URL.SIGN_IN} element={<SignIn />} />
-                            <Route path={PATH_URL.CART} element={<Cart />} />
+                                <Route element={<RequireAuth />}>
+                                    <Route path={PATH_URL.ACCOUNT.BASE} element={<Account />}>
+                                        <Route path={PATH_URL.ACCOUNT.PROFILE} element={<Profile />} />
+                                        <Route path={PATH_URL.ACCOUNT.ADDRESS} element={<Addresses />} />
+                                        <Route path={PATH_URL.ACCOUNT.ORDERS} element={<Orders />} />
+                                        <Route path={PATH_URL.ACCOUNT.FAVOURITES} element={<Favourite />} />
+                                    </Route>
 
-                            <Route element={<RequireAuth />}>
-                                <Route path={PATH_URL.ACCOUNT.BASE} element={<Account />}>
-                                    <Route path={PATH_URL.ACCOUNT.PROFILE} element={<Profile />} />
-                                    <Route path={PATH_URL.ACCOUNT.ADDRESS} element={<Addresses />} />
-                                    <Route path={PATH_URL.ACCOUNT.ORDERS} element={<Orders />} />
-                                    <Route path={PATH_URL.ACCOUNT.FAVOURITES} element={<Favourite />} />
                                 </Route>
+                                <Route path={PATH_URL.ADD_ADDRESS} element={<AddAddress />} />
+                                <Route path={PATH_URL.PRODUCT_DETAILS} element={<ProductDetails />} />
+                                <Route path={PATH_URL.CHECK_OUT} element={<CheckOut />} />
+                                <Route path="/orders/:orderNumber" element={<OrderSummary />} />
 
                             </Route>
-                            <Route path={PATH_URL.ADD_ADDRESS} element={<AddAddress />} />
-                            <Route path={PATH_URL.PRODUCT_DETAILS} element={<ProductDetails />} />
-                            <Route path={PATH_URL.CHECK_OUT} element={<CheckOut />} />
-                            <Route path="/orders/:orderNumber" element={<OrderSummary />} />
+                            <Route path={PATH_URL.EMAIL} element={<Email />} />
+                            <Route path={PATH_URL.RESETPASSWORD} element={<ResetPassword />} />
+                            <Route path={PATH_URL.FORGOT_PASSWORD} element={<ForgotPassword />} />
+                            <Route path={PATH_URL.SIGN_UP} element={<SignUp />} />
+                            <Route path='*' element={<NotFound />} />
 
-                        </Route>
-                        <Route path={PATH_URL.EMAIL} element={<Email />} />
-                        <Route path={PATH_URL.RESETPASSWORD} element={<ResetPassword />} />
-                        <Route path={PATH_URL.FORGOT_PASSWORD} element={<ForgotPassword />} />
-                        <Route path={PATH_URL.SIGN_UP} element={<SignUp />} />
-                        <Route path='*' element={<NotFound />} />
-
-                        {/* SELLER ROUTES */}
-                        <Route path={PATH_URL.SELL.LOG_IN} element={<Login />} />
-                        <Route element={<ShopRequireAuth />}>
-                            <Route path={PATH_URL.SELL.DASHBOARD} element={<SellDashBoard />}>
-                                <Route path={PATH_URL.SELL.PROFILE} element={<ShopProfile />} />
-                                <Route path={PATH_URL.SELL.ADD_PRODUCTS} element={<AddProduct />} />
-                                <Route path={PATH_URL.SELL.MY_PRODUCTS} element={<MyProducts />} />
-                                <Route path={PATH_URL.SELL.EDIT_PRODUCT} element={<EditProduct />} />
-                                <Route path={PATH_URL.SELL.ADD_CATEGORY} element={<AddCategory />} />
+                            {/* SELLER ROUTES */}
+                            <Route path={PATH_URL.SELL.LOG_IN} element={<Login />} />
+                            <Route element={<ShopRequireAuth />}>
+                                <Route path={PATH_URL.SELL.DASHBOARD} element={<SellDashBoard />}>
+                                    <Route path={PATH_URL.SELL.PROFILE} element={<ShopProfile />} />
+                                    <Route path={PATH_URL.SELL.ADD_PRODUCTS} element={<AddProduct />} />
+                                    <Route path={PATH_URL.SELL.MY_PRODUCTS} element={<MyProducts />} />
+                                    <Route path={PATH_URL.SELL.EDIT_PRODUCT} element={<EditProduct />} />
+                                    <Route path={PATH_URL.SELL.ADD_CATEGORY} element={<AddCategory />} />
+                                </Route>
                             </Route>
-                        </Route>
-                        <Route path={PATH_URL.SELL.CREATE_BUSINESS_ACCOUNT} element={<CreateAccount />} />
-                        <Route path={PATH_URL.SELL.FORGOT_PASSWORD} element={<ForgotPasswordShop />} />
-                        <Route path={PATH_URL.SELL.RESET_SHOP_PASSWORD} element={<ResetShopPassword />} />
-                    </Routes>
-                </ToastProvider>
+                            <Route path={PATH_URL.SELL.CREATE_BUSINESS_ACCOUNT} element={<CreateAccount />} />
+                            <Route path={PATH_URL.SELL.FORGOT_PASSWORD} element={<ForgotPasswordShop />} />
+                            <Route path={PATH_URL.SELL.RESET_SHOP_PASSWORD} element={<ResetShopPassword />} />
+                        </Routes>
+                    </ToastProvider>
+                </Suspense>
             </BrowserRouter>
         </>
     );
--- src/Features/customers/pages/ProductDetails.jsx
+++ src/Features/customers/pages/ProductDetails.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Link, useParams, } from 'react-router-dom';
+import { useEffect, useState, useCallback, useMemo } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import Button from '../../../components/Button';
 import { BASEURL, GET_ROUTES } from '../../../constant';
 import WishlistButton from '../../../components/WishlistButton';
@@ -10,43 +10,56 @@ import { useCart, useCustomerAuth } from '../../../hooks/useAppSelectors';
 import { addTocart } from '../../../redux/actionsCreators/cartAction';
 import { useDispatch } from 'react-redux';
 import axiosCustomer from '../../../utils/axiosCustomer';
-
+import Spinning from '../../../components/Spinning';
 
 const ProductDetails = () => {
     const { productId } = useParams();
-    const [product, setProduct] = useState()
+    const [product, setProduct] = useState(null);
     const [selectedImage, setSelectedImage] = useState(null);
-
 
     const { cartItem } = useCart();
     const { isAuthenticated } = useCustomerAuth();
     const dispatch = useDispatch();
 
-    console.log("PRUDUCT ID ::", productId);
-
-
     useEffect(() => {
-
+        let isMounted = true;
         const getProductDetails = async () => {
-            const response = await axiosCustomer.get(GET_ROUTES.GET_SINGLE_PRODUCT(productId));
-            if (response.data.success) {
-                setProduct(response.data.productDetail);
-                setSelectedImage(response.data.productDetail.image[0])
-
-            };
-        }
+            try {
+                const response = await axiosCustomer.get(GET_ROUTES.GET_SINGLE_PRODUCT(productId));
+                if (response.data.success && isMounted) {
+                    setProduct(response.data.productDetail);
+                    setSelectedImage(response.data.productDetail.image?.[0] || null);
+                }
+            } catch (error) {
+                // Optionally handle error
+            }
+        };
         getProductDetails();
+        return () => { isMounted = false; };
+    }, [productId]);
 
-    }, [productId, setProduct]);
+    const cartProduct = useMemo(
+        () => cartItem?.find(item => item.productId === productId),
+        [cartItem, productId]
+    );
 
+    const handleAddToCart = useCallback(() => {
+        if (isAuthenticated && productId) {
+            dispatch(addTocart(isAuthenticated, productId));
+        }
+    }, [dispatch, isAuthenticated, productId]);
 
-    const cartProduct = cartItem?.find(item => item.productId === productId);
+    const handleImageSelect = useCallback((img) => {
+        setSelectedImage(img);
+    }, []);
 
-    const handleClick = async () => {
-        dispatch(addTocart(isAuthenticated, productId));
-    };
-
-
+    if (!product) {
+        return (
+            <div className="flex justify-center items-center h-96">
+                <Spinning />
+            </div>
+        );
+    }
     return (
         <div>
             <section className="py-12 sm:py-16">
@@ -56,20 +69,27 @@ const ProductDetails = () => {
                             <div className="lg:flex lg:items-start">
                                 <div className="lg:order-2 lg:ml-5">
                                     <div className="max-w-xl overflow-hidden rounded-lg">
-                                        <img className="h-full w-full max-w-full object-cover" src={`${BASEURL}${selectedImage}`} alt="Product" />
+                                        <img
+                                            className="h-full w-full max-w-full object-cover"
+                                            src={selectedImage ? `${BASEURL}${selectedImage}` : ''}
+                                            alt="Product"
+                                        />
                                     </div>
                                 </div>
-
                                 <div className="mt-2 w-full lg:order-1 lg:w-32 lg:flex-shrink-0">
                                     <div className="flex flex-row items-start lg:flex-col">
-                                        {product?.image.map((img, index) => (
+                                        {product.image?.map((img, index) => (
                                             <button
-                                                key={index}
+                                                key={img}
                                                 type="button"
-                                                onClick={() => setSelectedImage(img)}
+                                                onClick={() => handleImageSelect(img)}
                                                 className={`flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 ${selectedImage === img ? 'border-gray-900' : 'border-transparent'}`}
                                             >
-                                                <img className="h-full w-full object-cover" src={`${BASEURL}${img}`} alt={`Thumbnail ${index + 1}`} />
+                                                <img
+                                                    className="h-full w-full object-cover"
+                                                    src={`${BASEURL}${img}`}
+                                                    alt={`Thumbnail ${index + 1}`}
+                                                />
                                             </button>
                                         ))}
                                     </div>
@@ -78,59 +98,58 @@ const ProductDetails = () => {
                         </div>
                         <div className="lg:col-span-2 lg:row-span-2 order-last sm:order-last md:order-first">
                             <div className="flex items-center justify-between">
-                                <h1 className="text-base font-bold text-gray-900 lg:text-2xl">{product?.productName}</h1>
-                                <WishlistButton productId={product?.id} />
+                                <h1 className="text-base font-bold text-gray-900 lg:text-base">{product.productName}</h1>
+                                <WishlistButton productId={product.id} />
                             </div>
                             <div className="mt-5 flex items-center space-x-2">
-                                <span className="text-base font-semibold text-gray-700">Brand:</span>
-                                <Link className="text-sm font-medium text-primary hover:underline">{product?.brand}</Link>
+                                <span className="text-sm font-semibold text-gray-700">Brand:</span>
+                                <Link className="text-sm font-medium text-primary hover:underline">{product.brand}</Link>
                             </div>
                             <div className="mt-10 flex flex-row items-center justify-between border-t border-b py-4 sm:space-x-4 sm:w-full">
                                 <div className="flex items-end">
-                                    <h1 className="text-base font-medium ">KSH {product?.price.toLocaleString()}</h1>
+                                    <h1 className="text-base font-medium ">KSH {product.price?.toLocaleString()}</h1>
                                 </div>
                                 {cartProduct ? (
-                                    <CartUpdateButton productId={product?.id} quantity={cartProduct?.quantity} productQuantity={product?.quantity} />
+                                    <CartUpdateButton
+                                        productId={product.id}
+                                        quantity={cartProduct.quantity}
+                                        productQuantity={product.quantity}
+                                    />
                                 ) : (
-                                    <Button label="Add to Cart" variant="primary" size="medium" onClick={handleClick} />
-                                )
-                                }
-
+                                    <Button
+                                        label="Add to Cart"
+                                        variant="primary"
+                                        size="medium"
+                                        onClick={handleAddToCart}
+                                    />
+                                )}
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <div className="container mx-auto mb-7 px-11 bg-white border rounded-lg shadow-sm">
-                <div className="m-5 ">
-
-
-                    <div className=" flex gap-10 border-b border-gray-300">
+                <div className="m-5">
+                    <div className="flex gap-10 border-b border-gray-300">
                         <h1 className="py-4 text-lg font-medium text-gray-900 hover:border-gray-400 hover:text-gray-800">
                             Description
                         </h1>
-
                     </div>
-                    <div className="mt-2 flow-root sm:mt-12">
-                        {product && product.id && <DisplayDescription description={product.description} />}
-
+                    <div className="mt-2 flow-root sm:mt-12 text-sm">
+                        {product.description && <DisplayDescription description={product.description} />}
                     </div>
                     <div className="border-b border-gray-300 mt-6">
-                        <h1 className=" py-4 text-lg font-medium text-gray-900 hover:text-gray-800">
+                        <h1 className="py-4 text-lg font-medium text-gray-900 hover:text-gray-800">
                             Key Features
                         </h1>
                     </div>
-                    <div className="mt-1 flow-root sm:mt-12 list-disc pl-5 text-gray-700">
-                        {product && product.id && <KeyFeaturesDisplay keyFeatures={product.keyFeatures} />}
+                    <div className="mt-1 flow-root sm:mt-12 list-disc pl-5 text-gray-700 text-sm">
+                        {product.keyFeatures && <KeyFeaturesDisplay keyFeatures={product.keyFeatures} />}
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default ProductDetails;
-
-
-
-
